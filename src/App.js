9d4f07822a43dedc9d21f1d53f361d27400328b9
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-29 13:26:47 -0800

    Update App links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import PlantProfiles from "./pages/PlantProfiles";
 
 function App() {
   return (
     <div className="page-container">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/plant-profiles" element={<PlantProfiles />}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/PlantProfiles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Searchbar from "../components/Searchbar";
+
+export default function PlantProfiles() {
+  return (
+    <div>
+      <main className="main">
+        <h1 className="pp--title">Plant Profiles</h1>
+        <Searchbar />
+      </main>
+    </div>
+  );
+}
