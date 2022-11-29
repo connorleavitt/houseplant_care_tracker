da22ef3dc646503afb75a8eb5fe2cd8ef74f82c7
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-29 14:51:53 -0800

    Update pp styles

--- src/pages/PlantProfiles.js
+++ src/pages/PlantProfiles.js
@@ -1,12 +1,20 @@
 import React from "react";
+import AddNew from "../components/AddNew";
+import PlantProfileCard from "../components/PlantProfileCard";
 import Searchbar from "../components/Searchbar";
 
 export default function PlantProfiles() {
   return (
-    <div>
-      <main className="main">
+    <div className="pp">
+      <main className="pp-main">
         <h1 className="pp--title">Plant Profiles</h1>
         <Searchbar />
+        <button>All</button>
+        <button>Indoor</button>
+        <button>Outdoor</button>
+        <button>Other</button>
+        <AddNew name={"Plant"} />
+        <PlantProfileCard />
       </main>
     </div>
   );
