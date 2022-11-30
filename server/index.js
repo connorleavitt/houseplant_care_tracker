093489dabf775397c7d7f5b603df3df16a83706d
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-30 11:51:13 -0800

    Set mongoose connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false, need to understand this a bit more - deprecated?
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// module.exports = app;
