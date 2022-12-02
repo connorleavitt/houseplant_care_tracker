ca76756f28c6942439e97fae33994ffb0bf28f33
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-12-02 15:12:51 -0800

    Update login styling for desk and mobile

--- client/src/scenes/loginPage/loginPage.jsx
+++ client/src/scenes/loginPage/loginPage.jsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 
 export default function LoginPage() {
   const theme = useTheme();
-  const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+  const isNonMobileScreens = useMediaQuery("(min-width: 800px)");
   return (
     <Box>
       <Box
@@ -17,7 +17,7 @@ export default function LoginPage() {
         </Typography>
       </Box>
       <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
+        width={isNonMobileScreens ? "30%" : "80%"}
         p="2rem"
         m="2rem auto"
         borderRadius="1.5rem"
