7dcf1ff454dee85ee7d493fef0fe9994baee113e
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-12-05 17:39:01 -0800

    Update init values for dynamic user id

--- client/src/scenes/plantProfilesPage/plantProfileForm.jsx
+++ client/src/scenes/plantProfilesPage/plantProfileForm.jsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
 import { Formik, Field, Form } from "formik";
 import * as yup from "yup";
-import FlexBetween from "components/FlexBetween";
-import Dropzone from "react-dropzone";
+// import FlexBetween from "components/FlexBetween";
+// import Dropzone from "react-dropzone";
 // import { DesktopDatePicker } from "@mui/x-date-pickers";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { setPlantProfiles } from "state";
+import { useSelector } from "react-redux";
 
 const plantProfileSchema = yup.object().shape({
   userId: yup.string().required("required"),
@@ -31,28 +30,6 @@ const plantProfileSchema = yup.object().shape({
   commonIssues: yup.string().required("required"),
   notes: yup.string().required("required"),
 });
-
-const initialValuesNewPlantProfile = {
-  userId: "",
-  plantName: "",
-  scientificName: "",
-  dateAcquired: "",
-  plantFamily: "",
-  iGotItFrom: "",
-  toxicity: "",
-  water: "",
-  light: "",
-  soilType: "",
-  humidity: "",
-  idealTemp: "",
-  fertilizationMethod: "",
-  fertilizationFrequency: "",
-  waterLevel: "",
-  sunlightLevel: "",
-  commonIssues: "",
-  notes: "",
-  picturePath: "",
-};
 
 // const savedValuesPlantProfile = {
 //   userId: "",
@@ -81,15 +58,12 @@ const initialValuesNewPlantProfile = {
 export default function PlantProfileForm() {
   const { _id } = useSelector((state) => state.user);
 
-  // const [formValues, setFormValues] = useState(null);
-
   const [error, setError] = useState(null);
   const { palette } = useTheme();
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const newPlantProfileForm = async (values, onSubmitProps) => {
-    // console.log("new plant:", values);
+    console.log("new plant:", values);
     console.log("new plant:", JSON.stringify(values));
 
     const savedUserResponse = await fetch(
@@ -102,44 +76,17 @@ export default function PlantProfileForm() {
     );
     // console.log(savedUserResponse);
     const savedPlantProfile = await savedUserResponse.json();
+    onSubmitProps.resetForm();
+
     if (!savedUserResponse.ok) {
       setError(savedPlantProfile.error);
+      console.log(error);
     }
 
     if (savedUserResponse.ok) {
       setError(null);
       console.log("New plant profile added", savedPlantProfile);
-    }
-
-    onSubmitProps.resetForm();
-
-    if (savedPlantProfile) {
-      // dispatch(
-      //   setPlantProfiles({
-      //     userId: savedPlantProfile.userId,
-      //     userFirstName: savedPlantProfile.userFirstName,
-      //     userPicturePath: savedPlantProfile.userPicturePath,
-      //     plantName: savedPlantProfile.plantName,
-      //     scientificName: savedPlantProfile.scientificName,
-      //     dateAcquired: savedPlantProfile.dateAcquired,
-      //     plantFamily: savedPlantProfile.plantFamily,
-      //     iGotItFrom: savedPlantProfile.iGotItFrom,
-      //     toxicity: savedPlantProfile.toxicity,
-      //     water: savedPlantProfile.water,
-      //     light: savedPlantProfile.light,
-      //     soilType: savedPlantProfile.soilType,
-      //     humidity: savedPlantProfile.humidity,
-      //     idealTemp: savedPlantProfile.idealTemp,
-      //     fertilizationMethod: savedPlantProfile.fertilizationMethod,
-      //     fertilizationFrequency: savedPlantProfile.fertilizationFrequency,
-      //     waterLevel: savedPlantProfile.waterLevel,
-      //     sunlightLevel: savedPlantProfile.sunlightLevel,
-      //     commonIssues: savedPlantProfile.commonIssues,
-      //     notes: savedPlantProfile.notes,
-      //     picturePath: savedPlantProfile.picturePath,
-      //   })
-      // );
-      navigate("/home");
+      navigate("/");
     }
   };
 
@@ -150,25 +97,34 @@ export default function PlantProfileForm() {
   return (
     <Formik
       onSubmit={handleFormSubmit}
-      // onSubmit={(values, { setSubmitting }) => {
-      //   setSubmitting(true);
-      //   //make async call
-      //   handleFormSubmit(values);
-      //   setSubmitting(false);
-      // }}
-      initialValues={initialValuesNewPlantProfile}
+      initialValues={{
+        userId: _id,
+        plantName: "",
+        scientificName: "",
+        dateAcquired: "",
+        plantFamily: "",
+        iGotItFrom: "",
+        toxicity: "",
+        water: "",
+        light: "",
+        soilType: "",
+        humidity: "",
+        idealTemp: "",
+        fertilizationMethod: "",
+        fertilizationFrequency: "",
+        waterLevel: "",
+        sunlightLevel: "",
+        commonIssues: "",
+        notes: "",
+        picturePath: "",
+      }}
+
       // validationSchema={plantProfileSchema}
     >
       {({ values, isSubmitting }) => (
         <Form>
           <Box display="flex" width="50%" flexDirection="column" gap="30px">
             <pre>{JSON.stringify(values, null, 2)}</pre>
-            <Field
-              placeholder="userId"
-              name="userId"
-              type="input"
-              as={TextField}
-            />
             <Field
               placeholder="plant name"
               name="plantName"
