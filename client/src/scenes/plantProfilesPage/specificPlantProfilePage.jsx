a1a96cf5ef57e47b2c9fba2d6a7f16943b4cc1ac
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-12-14 10:18:12 -0800

    Update mobile responsiveness with buttons

--- client/src/scenes/plantProfilesPage/specificPlantProfilePage.jsx
+++ client/src/scenes/plantProfilesPage/specificPlantProfilePage.jsx
@@ -1,14 +1,13 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, useMediaQuery } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import NavbarPage from "scenes/navbar/navbarPage";
 import SpecificPlantProfileWidget from "scenes/widgets/SpecificPlantProfileWidget";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { fontWeight } from "@mui/system";
 
 export default function SpecificPlantProfilePage() {
-  // const isNonMobileScreens = useMediaQuery("(min-width: 800px");
+  const isNonMobileScreens = useMediaQuery("(min-width: 800px");
   const { _id } = useSelector((state) => state.user);
 
   return (
@@ -16,21 +15,54 @@ export default function SpecificPlantProfilePage() {
       <NavbarPage userId={_id} />
       <Box
         display="flex"
+        flexDirection="column"
         justifyContent="center"
+        alignItems="center"
         width="100%"
-        mt="2rem"
-        gap="2rem"
+        // gap="2rem"
       >
-        <Button sx={{ fontSize: "1.5rem" }}>
-          <ArrowBackIcon sx={{ fontSize: "1.75rem" }} />
-          Previous
-        </Button>
-        <Button sx={{ fontSize: "1.5rem" }}>
-          Next
-          <ArrowForwardIcon sx={{ fontSize: "1.75rem" }} />
-        </Button>
+        {isNonMobileScreens ? (
+          <>
+            <Box
+              display="flex"
+              justifyContent="center"
+              width="50%"
+              mt="2rem"
+              gap="2rem"
+            >
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                <ArrowBackIcon sx={{ fontSize: "1.5rem" }} />
+                Previous
+              </Button>
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                Next
+                <ArrowForwardIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+            </Box>
+            <SpecificPlantProfileWidget userId={_id} />
+          </>
+        ) : (
+          <>
+            <Box
+              display="flex"
+              justifyContent="center"
+              width="100%"
+              mt="2rem"
+              gap="2rem"
+            >
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                <ArrowBackIcon sx={{ fontSize: "1.5rem" }} />
+                Previous
+              </Button>
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                Next
+                <ArrowForwardIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+            </Box>
+            <SpecificPlantProfileWidget userId={_id} />
+          </>
+        )}
       </Box>
-      <SpecificPlantProfileWidget userId={_id} />
     </Box>
   );
 }
