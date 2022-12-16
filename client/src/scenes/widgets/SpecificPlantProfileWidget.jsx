4e62f0a922dd6bd5e02f6b1d31967ae1f4c9718c
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-12-15 16:09:01 -0800

    Update mobile delete confirm logic styling

--- client/src/scenes/widgets/SpecificPlantProfileWidget.jsx
+++ client/src/scenes/widgets/SpecificPlantProfileWidget.jsx
@@ -225,76 +225,142 @@ const SpecificPlantProfileWidget = ({ isProfile = false }) => {
       ) : (
         // MOBILE
         <>
-          <Box
-            display="flex"
-            justifyContent="center"
-            width="100%"
-            mt="2rem"
-            gap="2rem"
-          >
-            <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
-              <ArrowBackIcon sx={{ fontSize: "1.5rem" }} />
-              Previous
-            </Button>
-            <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
-              Next
-              <ArrowForwardIcon sx={{ fontSize: "1.5rem" }} />
-            </Button>
+          <Box display={deletionConfirmation ? "none" : ""}>
+            <Box
+              display="flex"
+              justifyContent="center"
+              width="100%"
+              mt="2rem"
+              gap="2rem"
+            >
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                <ArrowBackIcon sx={{ fontSize: "1.5rem" }} />
+                Previous
+              </Button>
+              <Button className="spp--change-btns" sx={{ fontSize: "1.25rem" }}>
+                Next
+                <ArrowForwardIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+            </Box>
+            {plantProfiles.map(
+              ({
+                _id,
+                userId,
+                userFirstName,
+                userPicturePath,
+                picturePath,
+                plantName,
+                scientificName,
+                dateAcquired,
+                plantFamily,
+                iGotItFrom,
+                toxicity,
+                water,
+                light,
+                soilType,
+                humidity,
+                idealTemp,
+                currentLocation,
+                fertilizationMethod,
+                fertilizationFrequency,
+                waterLevel,
+                sunlightLevel,
+                commonIssues,
+                notes,
+              }) => (
+                <PlantProfileCardWidget
+                  key={_id}
+                  plantProfileId={_id}
+                  plantProfileUserId={userId}
+                  userFirstName={userFirstName}
+                  plantName={plantName}
+                  scientificName={scientificName}
+                  picturePath={picturePath}
+                  userPicturePath={userPicturePath}
+                  dateAcquired={dateAcquired}
+                  plantFamily={plantFamily}
+                  iGotItFrom={iGotItFrom}
+                  toxicity={toxicity}
+                  water={water}
+                  light={light}
+                  soilType={soilType}
+                  humidity={humidity}
+                  idealTemp={idealTemp}
+                  fertilizationMethod={fertilizationMethod}
+                  fertilizationFrequency={fertilizationFrequency}
+                  currentLocation={currentLocation}
+                  waterLevel={waterLevel}
+                  sunlightLevel={sunlightLevel}
+                  commonIssues={commonIssues}
+                  notes={notes}
+                />
+              )
+            )}
+            <Box
+              display="flex"
+              justifyContent="center"
+              width="100%"
+              m="2rem 0"
+              gap="2rem"
+            >
+              <Button
+                className="spp--update-btn"
+                sx={{ fontSize: "1.25rem" }}
+                onClick={() => navigate(`edit`)}
+              >
+                Update
+                <EditIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+              <Button
+                className="spp--delete-btn"
+                sx={{ fontSize: "1.25rem" }}
+                onClick={() => setDeletionConfirmation(true)}
+              >
+                Delete
+                <DeleteForeverIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+            </Box>
           </Box>
-          {plantProfiles.map(
-            ({
-              _id,
-              userId,
-              userFirstName,
-              userPicturePath,
-              picturePath,
-              plantName,
-              scientificName,
-              dateAcquired,
-              plantFamily,
-              iGotItFrom,
-              toxicity,
-              water,
-              light,
-              soilType,
-              humidity,
-              idealTemp,
-              currentLocation,
-              fertilizationMethod,
-              fertilizationFrequency,
-              waterLevel,
-              sunlightLevel,
-              commonIssues,
-              notes,
-            }) => (
-              <PlantProfileCardWidget
-                key={_id}
-                plantProfileId={_id}
-                plantProfileUserId={userId}
-                userFirstName={userFirstName}
-                plantName={plantName}
-                scientificName={scientificName}
-                picturePath={picturePath}
-                userPicturePath={userPicturePath}
-                dateAcquired={dateAcquired}
-                plantFamily={plantFamily}
-                iGotItFrom={iGotItFrom}
-                toxicity={toxicity}
-                water={water}
-                light={light}
-                soilType={soilType}
-                humidity={humidity}
-                idealTemp={idealTemp}
-                fertilizationMethod={fertilizationMethod}
-                fertilizationFrequency={fertilizationFrequency}
-                currentLocation={currentLocation}
-                waterLevel={waterLevel}
-                sunlightLevel={sunlightLevel}
-                commonIssues={commonIssues}
-                notes={notes}
-              />
-            )
-          )}
+          <Box
+            display={deletionConfirmation ? "flex" : "none"}
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            m="10rem auto"
+            width="90%"
+            border="5px solid gray"
+            borderRadius="10px"
+            p="1rem"
+          >
+            <Typography p="1rem" fontSize="1.5rem" width="100%">
+              Are you sure you want to delete this plant profile?
+            </Typography>
+            <Typography p="0 1rem 1rem 1rem" fontSize="1rem" width="100%">
+              Once it is deleted, it will be gone forever!
+            </Typography>
+            <Box
+              display="flex"
+              justifyContent="space-around"
+              width="100%"
+              m="1rem 0"
+            >
+              <Button
+                className="spp--update-btn"
+                sx={{ fontSize: "1.25rem" }}
+                onClick={() => setDeletionConfirmation(false)}
+              >
+                CANCEL
+              </Button>
+              <Button
+                className="spp--delete-btn"
+                sx={{ fontSize: "1.25rem" }}
+                onClick={handleDelete}
+              >
+                DELETE
+                <DeleteForeverIcon sx={{ fontSize: "1.5rem" }} />
+              </Button>
+            </Box>
+          </Box>
         </>
       )}
     </>
