e4824405f91e361b3298637ccb8cf745ae393938
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-12-16 16:22:42 -0800

    Update for style for mobile

--- client/src/scenes/userProfilePage/userProfilePage.jsx
+++ client/src/scenes/userProfilePage/userProfilePage.jsx
@@ -8,33 +8,28 @@ import {
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
 import { setLogout, setUser } from "state";
 import NavbarPage from "scenes/navbar/navbarPage";
 import UserWidget from "scenes/widgets/UserWidget";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
-
-import FlexBetween from "components/FlexBetween";
 import { useNavigate } from "react-router";
 import moment from "moment";
 
 export default function UserProfilePage() {
   const [currentUser, setCurrentUser] = useState(null);
   const { _id, picturePath } = useSelector((state) => state.user);
+  const plantProfiles = useSelector((state) => state.plantProfiles);
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width: 800px)");
 
   const theme = useTheme();
-  const neutralMedium = theme.palette.neutral.medium;
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
   const primaryDark = theme.palette.primary.dark;
-  const primaryMain = theme.palette.primary.main;
-  // const alt = theme.palette.background.alt;
+  const widgetColor = theme.palette.background.alt;
 
   const getUser = async () => {
     const res = await fetch(`http://localhost:3001/users/${_id}`, {
@@ -43,7 +38,6 @@ export default function UserProfilePage() {
     });
     const data = await res.json();
     setCurrentUser(data);
-    // console.log(data);
     dispatch(
       setUser({
         user: data,
@@ -70,125 +64,306 @@ export default function UserProfilePage() {
     <Box>
       <NavbarPage userId={_id} />
       <Box display="flex">
-        <Box
-          m="2rem"
-          p="0 2rem"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          flexBasis={isNonMobileScreens ? "30%" : undefined}
-        >
-          <UserWidget userId={_id} picturePath={picturePath} />
-          <Box
-            m="0.5rem"
-            p="0"
-            borderRadius="10px"
-            backgroundColor={neutralLight}
-            display="flex"
-            flexDirection="column"
-            alignItems="flex-end"
-            sx={{ width: "100%" }}
-          >
+        {isNonMobileScreens ? (
+          <>
             <Box
-              m="1rem"
-              p="0"
-              gap="1rem"
+              m="2rem"
+              p="0 2rem"
               display="flex"
               flexDirection="column"
-              alignItems="flex-end"
+              alignItems="center"
+              flexBasis={isNonMobileScreens ? "30%" : undefined}
             >
+              <UserWidget userId={_id} picturePath={picturePath} />
               <Box
-                m="0"
+                mt="1rem"
                 p="0"
+                borderRadius="10px"
+                backgroundColor={widgetColor}
                 display="flex"
                 flexDirection="column"
                 alignItems="flex-end"
+                sx={{ width: "100%" }}
               >
-                Created On:{" "}
-                {moment(currentUser.createdAt).format("MMM Do (YYYY)")}
+                <Box
+                  m="1rem"
+                  p="0"
+                  gap="1rem"
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="flex-end"
+                  width="calc(100% - 2rem)"
+                >
+                  <Box
+                    m="0"
+                    p="0"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Typography>Created On:</Typography>
+                    <Typography sx={{ ml: "1rem", width: "150px" }}>
+                      <strong>
+                        {moment(currentUser.createdAt).format("MMM Do (YYYY)")}
+                      </strong>
+                    </Typography>
+                  </Box>
+                  <Box
+                    m="0"
+                    p="0"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Typography>Account Age:</Typography>
+                    <Typography sx={{ ml: "1rem", width: "150px" }}>
+                      <strong>{getAge(currentUser.createdAt)} days old</strong>
+                    </Typography>
+                  </Box>
+                </Box>
               </Box>
+
               <Box
-                m="0"
-                p="0"
                 display="flex"
                 flexDirection="column"
-                alignItems="flex-end"
+                alignItems="center"
+                mb="1rem"
+                sx={{ width: "100%" }}
               >
-                Account Age: {getAge(currentUser.createdAt)} days old
+                <Button
+                  fullWidth
+                  onClick={() => navigate(`edit`)}
+                  sx={{
+                    m: "1rem",
+                    p: "0.25rem 0.5rem",
+                    borderRadius: "5px",
+                    color: dark,
+                    border: `2px dashed ${primaryDark}`,
+                    "&:hover": {
+                      color: background,
+                      backgroundColor: primaryDark,
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  <EditOutlinedIcon sx={{ mr: "1rem" }} />
+                  <Typography fontSize="1rem">Edit Profile</Typography>
+                </Button>
+                <Button
+                  fullWidth
+                  onClick={() => dispatch(setLogout())}
+                  sx={{
+                    m: "0",
+                    p: "0.25rem 0.5rem",
+                    borderRadius: "5px",
+                    color: dark,
+                    border: `2px solid ${dark}`,
+                    "&:hover": {
+                      color: background,
+                      backgroundColor: dark,
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  <Typography fontSize="1rem">Log Out</Typography>
+                </Button>
               </Box>
             </Box>
             <Box
               display="flex"
               flexDirection="column"
               alignItems="center"
-              mb="1rem"
-              sx={{ width: "100%" }}
+              m="2rem"
+              ml="0"
+              flexBasis={isNonMobileScreens ? "70%" : undefined}
             >
-              <Button
-                fullWidth
-                onClick={() => navigate(`edit`)}
-                sx={{
-                  m: "0",
-                  mb: "1rem",
-                  p: "0.25rem 0.5rem",
-                  borderRadius: "0",
-                  backgroundColor: neutralLight,
-                  color: dark,
-                  // borderColor: "red",
-                  "&:hover": {
-                    color: background,
-                    backgroundColor: primaryDark,
-                    cursor: "pointer",
-                  },
-                }}
+              <Box
+                display="flex"
+                flexDirection="column"
+                m="0"
+                p="1rem"
+                width="calc(100% - 4rem)"
+                backgroundColor={neutralLight}
+                borderRadius="10px"
               >
-                <EditOutlinedIcon sx={{ mr: "1rem" }} />
-                <Typography fontSize="1rem">Edit Profile</Typography>
-              </Button>
-              <Button
-                fullWidth
-                onClick={() => dispatch(setLogout())}
-                sx={{
-                  m: "0",
-                  p: "0.25rem 0.5rem",
+                <Typography sx={{ fontSize: "3rem" }}>STATS</Typography>
 
-                  borderRadius: "0",
-                  color: dark,
-                  backgroundColor: neutralLight,
-                  // borderColor: "red",
-                  "&:hover": {
-                    color: background,
-                    backgroundColor: dark,
-                    cursor: "pointer",
-                  },
-                }}
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </>
+        ) : (
+          <Box display="flex" flexDirection="column" sx={{ width: "100%" }}>
+            <Box
+              m="2rem"
+              p="0 2rem"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <UserWidget userId={_id} picturePath={picturePath} />
+              <Box
+                mt="1rem"
+                p="0"
+                borderRadius="10px"
+                backgroundColor={widgetColor}
+                display="flex"
+                flexDirection="column"
+                alignItems="flex-end"
+                sx={{ width: "100%" }}
               >
-                <Typography fontSize="1rem">Log Out</Typography>
-              </Button>
+                <Box
+                  m="1rem"
+                  p="0"
+                  gap="1rem"
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="flex-end"
+                  width="calc(100% - 2rem)"
+                >
+                  <Box
+                    m="0"
+                    p="0"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Typography>Created On:</Typography>
+                    <Typography sx={{ ml: "1rem", width: "150px" }}>
+                      <strong>
+                        {moment(currentUser.createdAt).format("MMM Do (YYYY)")}
+                      </strong>
+                    </Typography>
+                  </Box>
+                  <Box
+                    m="0"
+                    p="0"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Typography>Account Age:</Typography>
+                    <Typography sx={{ ml: "1rem", width: "150px" }}>
+                      <strong>{getAge(currentUser.createdAt)} days old</strong>
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+
+              <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                mb="1rem"
+                sx={{ width: "100%" }}
+              >
+                <Button
+                  fullWidth
+                  onClick={() => navigate(`edit`)}
+                  sx={{
+                    m: "1rem",
+                    p: "0.25rem 0.5rem",
+                    borderRadius: "5px",
+                    color: dark,
+                    border: `2px dashed ${primaryDark}`,
+                    "&:hover": {
+                      color: background,
+                      backgroundColor: primaryDark,
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  <EditOutlinedIcon sx={{ mr: "1rem" }} />
+                  <Typography fontSize="1rem">Edit Profile</Typography>
+                </Button>
+                <Button
+                  fullWidth
+                  onClick={() => dispatch(setLogout())}
+                  sx={{
+                    m: "0",
+                    p: "0.25rem 0.5rem",
+                    borderRadius: "5px",
+                    color: dark,
+                    border: `2px solid ${dark}`,
+                    "&:hover": {
+                      color: background,
+                      backgroundColor: dark,
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  <Typography fontSize="1rem">Log Out</Typography>
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              m="2rem"
+            >
+              <Box
+                display="flex"
+                flexDirection="column"
+                m="0"
+                p="1rem"
+                width="calc(100% - 4rem)"
+                backgroundColor={neutralLight}
+                borderRadius="10px"
+              >
+                <Typography sx={{ fontSize: "3rem" }}>STATS</Typography>
+
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    Number of Plant Profiles:
+                  </Typography>
+                  <Typography sx={{ fontSize: "2rem", ml: "2rem" }}>
+                    {plantProfiles.length}
+                  </Typography>
+                </Box>
+              </Box>
             </Box>
           </Box>
-        </Box>
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          m="2rem"
-          ml="0"
-          flexBasis={isNonMobileScreens ? "70%" : undefined}
-        >
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            m="0"
-            p="5rem"
-            width="calc(100% - 4rem)"
-            backgroundColor="red"
-            borderRadius="10px"
-          >
-            blank
-          </Box>
-        </Box>
+        )}
       </Box>
     </Box>
   );
